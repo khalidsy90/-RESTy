addcbe7874d07a2d6ad80aa020ea48aaf9493a9a
khalidsy90 <khalidhamedi3@gmail.com>
2021-12-06 01:29:54 +0200

    main files

---
+++ src/components/footer/index.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+
+export default function footer() {
+    return <footer>&copy; 2018</footer>;
+}
---
+++ src/components/headers/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function headers() {
+    return (
+      <header>
+        <h1>RESTy</h1>
+      </header>
+    );
+}
